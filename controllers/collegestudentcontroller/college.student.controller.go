824e9ce875fdf3b3d6bbda58965437f1b28f48de
mhdianrush <mhdianrush@gmail.com>
2023-07-07 23:23:26 +0700

    Develop for FindAll Feature and Index() and GetData() function

--- controllers/collegestudentcontroller/college.student.controller.go
+++ controllers/collegestudentcontroller/college.student.controller.go
@@ -1,10 +1,26 @@
 package collegestudentcontroller
 
 import (
-	"fmt"
+	"bytes"
+	"html/template"
 	"net/http"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Server Running")
+	temp, err := template.ParseFiles("views/collegestudent/index.html")
+	if err != nil {
+		panic(err)
+	}
+	temp.Execute(w, nil)
+}
+
+// we'll use on Index and Delete Data
+func GetData() string {
+	buffer := &bytes.Buffer{}
+
+	template.New("data.html").Funcs(template.FuncMap{
+		"increment": func(a, b int) int {
+			return a + b
+		},
+	}).ParseFiles("views/collegestudent/data.html")
 }
--- models/collegestudentmodel/college.student.model.go
+++ models/collegestudentmodel/college.student.model.go
@@ -1,2 +1,48 @@
 package collegestudentmodel
 
+import (
+	"database/sql"
+
+	"github.com/mhdianrush/go-crud-modal-bootstrap-ajax/config"
+	"github.com/mhdianrush/go-crud-modal-bootstrap-ajax/entities"
+)
+
+type CollegeStudentModel struct {
+	DB *sql.DB
+}
+
+func NewCollegeStudentModel() *CollegeStudentModel {
+	db, err := config.ConnectDB()
+	if err != nil {
+		panic(err)
+	}
+
+	return &CollegeStudentModel{
+		DB: db,
+	}
+}
+
+func (c *CollegeStudentModel) FindAll(CollegeStudent *[]entities.CollegeStudent) error {
+	rows, err := c.DB.Query("select * from CollegeStudent")
+	if err != nil {
+		return err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var CollegeStudentData entities.CollegeStudent
+		rows.Scan(
+			&CollegeStudentData.Id,
+			&CollegeStudentData.FullName,
+			&CollegeStudentData.Gender,
+			&CollegeStudentData.BirthPlace,
+			&CollegeStudentData.BirthDay,
+			&CollegeStudentData.Address,
+		)
+		*CollegeStudent = append(*CollegeStudent, CollegeStudentData)
+	}
+	return nil
+}
+
+
